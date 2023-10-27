16b198f9c7416dce04a5903ae3bca1a1008ec26a
EPIAd <138433736+EPIAd@users.noreply.github.com>
2023-10-27 12:02:29 +0900

    Update data.js
    금 비트코인만 시험삼아

--- js/data.js
+++ js/data.js
@@ -2,8 +2,8 @@ const qnaList = [
   {
     q: '1. 둘 중 하나만 투자한다면?',
     a: [
-      { answer: 'a. 금', score: 0, score2: 0, score3: 0, score4: 0, score5: 0 },
-      { answer: 'b. 비트코인', score: 10, score2: 0, score3: 0, score4: 0, score5: 0 }
+      { answer: 'a. 금 93.85% ', score: 0, score2: 0, score3: 0, score4: 0, score5: 0 },
+      { answer: 'b. 비트코인 49.95%', score: 10, score2: 0, score3: 0, score4: 0, score5: 0 }
     ]
   },
   {
